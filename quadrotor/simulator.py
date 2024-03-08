202902f57fc278772a1f6061b06e538db88217f7
rvorster <royvorster@gmail.com>
2024-03-08 09:08:58 -0800

    Allow initial state in simulation

--- quadrotor/simulator.py
+++ quadrotor/simulator.py
@@ -25,6 +25,7 @@ class SimulatorBase:
         dynamics: QuadrotorDynamicsBase,
         controller: ControllerBase,
         trajectory: TrajectoryBase,
+        initial_state: T.Optional[QuadrotorState] = None,
         t_total: float = 5.0,  # Simulation duration [s]
     ) -> None:
         self.dynamics = dynamics
@@ -36,13 +37,21 @@ class SimulatorBase:
 
         self.t_total = t_total
 
+        # Set the initial state to be the start of the trajectory if None
+        if initial_state is None:
+            traj = self.trajectory.eval(0.0)
+            self.initial_state = QuadrotorState(position=traj.position, velocity=traj.velocity)
+        else:
+            self.initial_state = initial_state
+
     def simulate(self) -> T.List[SimulatorState]:
         output: T.List[SimulatorState] = []
 
         # Set simulation time and state to zero
-        t, state = 0.0, self.trajectory.eval(0.0)
+        t, trajectory = 0.0, self.trajectory.eval(0.0)
 
         # Initialize the dynamics
+        state = self.initial_state
         self.dynamics.reset(state)
 
         t = 0.0
